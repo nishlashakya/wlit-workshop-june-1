7a893a07ef84e477e86e790d090a7eb036602dd7
nishlashakya <nishlashakya2@gmail.com>
2019-06-01 06:52:44 +0545

    Movie list component, css update

---
+++ src/components/MovieList.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react'
+import {snakeCase} from 'lodash'
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          movies: []
+        }
+        this.serverURL = 'http://localhost:4000/movies/'
+    }
+
+    componentDidMount() {
+        fetch(this.serverURL)
+        .then((data => data.json()))
+        .then((movies) => {
+            this.setState({
+                movies: movies
+            })
+        })
+    }
+
+    handleDelete = (id) => {
+      fetch(this.serverURL + id, { method: 'DELETE' });
+      const movies = this.state.movies;
+      const position = movies.findIndex((movie) => id === movie.id);
+      movies.splice(position, 1);
+      this.setState({ movies });
+    }
+
+    render() {
+        return (
+          <div className="container">
+            <div className="row">
+                {this.state.movies.map((movie) =>
+                  <div className="col-sm" key={movie.id}>
+                    <div className="card movie-card">
+                      <img src={`/images/${snakeCase(movie.name)}.jpg`} className="card-img-top" alt="..." />
+                      <div className="card-body">
+                        <h5 className="card-title">{movie.name}</h5>
+                        <p className="card-text">Cast: {movie.cast}</p>
+                        <p className="card-text">Genre: {movie.genre}</p>
+                        <button className="btn btn-primary button">Edit</button>
+                        <button className="btn btn-danger button">Delete</button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+            </div>
+          </div>
+        )
+    }
+}
+
+export default Home
